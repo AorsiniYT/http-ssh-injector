401ac3a4aa67321be4695df475b78d0d3350daa0
AorsiniYT <albertorsini12@gmail.com>
2022-06-17 09:59:47 -0500

    main

--- jstest.js
+++ jstest.js
@@ -1,9 +1,9 @@
 const sleepNow = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 
 async function repeatedGreetingsLoop() {
-  for (let i = 1; i <= 5; i++) {
-    await sleepNow(1000)
-    console.log(`Hello #${i}`)
+  for (let i = 1; i <= 1; i++) {
+    await sleepNow(3000)
+    $("button.btn.btn-block.btn-primary").click();
   }
 }
 
